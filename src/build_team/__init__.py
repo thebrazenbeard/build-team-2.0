ce34438215ec3a3a09f6d24fe2d5fd76447f2fab
thebrazenbeard <patrickallynsims@gmail.com>
2026-08-04 06:33:17 -0400

    build: add package entrypoint

---
+++ src/build_team/__init__.py
@@ -0,0 +1,5 @@
+"""Build Team 2.0: ten cognitive facets, one shared hivemind."""
+
+from .models import CollectiveDecision, HivemindSnapshot, Perspective
+
+__all__ = ["CollectiveDecision", "HivemindSnapshot", "Perspective"]
